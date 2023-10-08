f0610a9c9ce402271ec4a0cada429f0bf5cea1e3
Kevin Biju Kulangara <118618402+Kevin1077@users.noreply.github.com>
2023-10-08 13:15:15 +0530

    Add files via upload
     digits addition from two digit number input

---
+++ main.py
@@ -0,0 +1,14 @@
+# to input a two digit number and store it in a variable as string
+two_digit_num=input("Enetr a two digit number\n")
+
+#to take first number from the inputed string and convert it to integer type
+first_digit=int(two_digit_num[0])
+
+#to take second number from the inputed string and convert it to integer type
+second_digit=int(two_digit_num[1])
+
+#to find sum 
+sum=first_digit+second_digit
+
+#to print sum
+print(sum)
