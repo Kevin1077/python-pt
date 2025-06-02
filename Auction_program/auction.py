4d49a6dcac0bded44991e5d2dbabe12cea558224
Kevin Biju Kulangara <118618402+Kevin1077@users.noreply.github.com>
2025-06-02 15:29:51 +0530

    Create auction.py

---
+++ Auction_program/auction.py
@@ -0,0 +1,30 @@
+
+import art
+
+print(art.logo)
+
+decision = "yes"
+auction_dic = {}
+
+def high_bid(dic):
+    highest = 0
+    bidder = ""
+    for key in dic:
+        bid_amount = (dic[key])
+        if bid_amount >= highest:
+            highest = bid_amount
+            bidder = key
+
+    print(f"The highest bid is {highest} from the bidder {bidder}")
+
+while decision == "yes":
+    name = input("Enter your name\n")
+    bid  = int(input("Enter your bid\n"))
+    auction_dic[name] = bid
+    decision = input("Do you want to continue (yes/no)")
+    if decision == "no":
+        high_bid(auction_dic)
+        break
+
+    elif decision == "yes":
+        print("\n" * 100)
