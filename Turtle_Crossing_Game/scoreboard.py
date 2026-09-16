df460634cc3df1c3023a2fa6fc50b4625588ace9
Kevin Biju Kulangara <118618402+Kevin1077@users.noreply.github.com>
2026-09-16 15:21:34 +0530

    Add Scoreboard class for game scoring

---
+++ Turtle_Crossing_Game/scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+FONT = ("Courier", 24, "bold")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(-220, 250)
+        self.level = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def update_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("Game Over", align="center", font=FONT)
+
+
