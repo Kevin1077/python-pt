4bd5795bca5a262849de6758163b20510ddc3efd
Kevin Biju Kulangara <118618402+Kevin1077@users.noreply.github.com>
2026-09-17 12:03:38 +0530

    Implement high score tracking in scoreboard
    Added high score functionality and reset method.

--- snake game/scoreboard.py
+++ snake game/scoreboard.py
@@ -11,18 +11,28 @@ class Score(Turtle):
         self.color("white")
         self.penup()
         self.goto(0, 260)
+
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+
         self.score = 0
         self.update_score()
         self.hideturtle()
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+
+        self.score = 0
+        self.update_score()
 
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.update_score()
